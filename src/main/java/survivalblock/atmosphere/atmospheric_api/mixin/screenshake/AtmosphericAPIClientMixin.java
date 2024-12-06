9b3d11e15e723ba107bd469699271db8ab8566e1
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2024-12-05 22:49:03 -0800

    Remap fail

--- src/main/java/survivalblock/atmosphere/atmospheric_api/mixin/screenshake/AtmosphericAPIClientMixin.java
+++ src/main/java/survivalblock/atmosphere/atmospheric_api/mixin/screenshake/AtmosphericAPIClientMixin.java
@@ -4,14 +4,15 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import survivalblock.atmosphere.atmospheric_api.not_mixin.AtmosphericAPIClient;
 import survivalblock.atmosphere.atmospheric_api.not_mixin.screenshake.ScreenShaker;
 
-@Mixin(AtmosphericAPIClient.class)
+@Mixin(value = AtmosphericAPIClient.class, remap = false)
 public class AtmosphericAPIClientMixin {
 
     @Inject(method = "onInitializeClient", at = @At("HEAD"))
-    private void tickActiveScreenShaker() {
+    private void tickActiveScreenShaker(CallbackInfo ci) {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             ScreenShaker active = ScreenShaker.getActiveInstance();
             if (active != null && !active.hasEnded()) active.tick();
