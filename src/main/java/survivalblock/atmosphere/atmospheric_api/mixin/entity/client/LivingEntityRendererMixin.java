1a9ecc17255280fd836fe9f50786d3ab2ed6ab8c
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2025-05-12 17:35:17 -0700

    Actually add mixins for EmptyModelRenderer

---
+++ src/main/java/survivalblock/atmosphere/atmospheric_api/mixin/entity/client/LivingEntityRendererMixin.java
@@ -0,0 +1,28 @@
+package survivalblock.atmosphere.atmospheric_api.mixin.entity.client;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.client.render.entity.model.EntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import survivalblock.atmosphere.atmospheric_api.not_mixin.entity.client.EmptyModelRenderer;
+
+@Mixin(LivingEntityRenderer.class)
+public class LivingEntityRendererMixin {
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    @WrapOperation(method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/model/EntityModel;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;III)V"))
+    private <T extends LivingEntity> void renderEmpty(EntityModel<T> instance, MatrixStack matrixStack, VertexConsumer vertexConsumer, int light, int overlay, int color, Operation<Void> original, @Local(argsOnly = true) T living, @Local(argsOnly = true)VertexConsumerProvider vertexConsumerProvider, @Local(argsOnly = true, ordinal = 1) float tickDelta) {
+        if ((LivingEntityRenderer) (Object) this instanceof EmptyModelRenderer emptyModelRenderer) {
+            emptyModelRenderer.renderWithEntityData(living, matrixStack, vertexConsumerProvider, tickDelta, light, overlay, color);
+        } else {
+            original.call(instance, matrixStack, vertexConsumer, light, overlay, color);
+        }
+    }
+}
