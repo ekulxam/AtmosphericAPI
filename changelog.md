391aa1cf8def5636103d33fb7e6254d9cb064199
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2026-01-22 22:58:47 -0800

    3.2.1 make `addEnchantedStack` `public`

--- changelog.md
+++ changelog.md
@@ -1,5 +1,2 @@
-Atmospheric API 3.2.0
-- Remove InGameHudAccessor calls from IAmASpyglassItem
-- Rename mixin package survivalblock.atmosphere.atmospheric_api.mixin.resourcepack to survivalblock.atmosphere.atmospheric_api.mixin.resource
-- Add AdditionalSplashTextRegistry
-- Add CreativeTabEnchantmentAdder
+Atmospheric API 3.2.1
+- Cleanup CreativeTabEnchantmentAdder and make its method public
--- src/main/java/survivalblock/atmosphere/atmospheric_api/not_mixin/item/CreativeTabEnchantmentAdder.java
+++ src/main/java/survivalblock/atmosphere/atmospheric_api/not_mixin/item/CreativeTabEnchantmentAdder.java
@@ -11,40 +11,41 @@ import net.minecraft.world.item.enchantment.Enchantment;
 import org.jetbrains.annotations.Nullable;
 import survivalblock.atmosphere.atmospheric_api.not_mixin.AtmosphericAPI;
 
-import java.util.Objects;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.Optional;
 import java.util.function.Consumer;
 
 @SuppressWarnings("unused")
 public class CreativeTabEnchantmentAdder {
 
-    private static void addEnchantedStack(Item item, CreativeModeTab.ItemDisplayParameters displayContext, String enchantmentId, CreativeModeTab.Output entries) {
+    public static void addEnchantedStack(Item item, CreativeModeTab.ItemDisplayParameters displayContext, String enchantmentId, CreativeModeTab.Output entries) {
         addEnchantedStack(item, displayContext, enchantmentId, entries, null);
     }
 
-    private static void addEnchantedStack(Item item, CreativeModeTab.ItemDisplayParameters displayContext, String enchantmentId, CreativeModeTab.Output entries, @Nullable Consumer<ItemStack> stackModifier) {
+    public static void addEnchantedStack(Item item, CreativeModeTab.ItemDisplayParameters displayContext, String enchantmentId, CreativeModeTab.Output entries, @Nullable Consumer<ItemStack> stackModifier) {
         try {
             ItemStack stack = new ItemStack(item);
-            AtomicReference<Holder<Enchantment>> reference = new AtomicReference<>(null);
             ResourceLocation id = ResourceLocation.parse(enchantmentId);
-            displayContext.holders()
+
+            Optional<Holder.Reference<Enchantment>> optional = displayContext.holders()
                     .lookupOrThrow(Registries.ENCHANTMENT)
                     .listElements()
                     .filter(enchantmentRef -> enchantmentRef.is(id))
-                    .findFirst()
-                    .ifPresent(reference::set);
-            Holder<Enchantment> enchantmentEntry = Objects.requireNonNull(reference.get());
+                    .findFirst();
+            if (optional.isEmpty()) {
+                throw new NullPointerException("Enchantment " + enchantmentId + " was not found");
+            }
+
+            Holder<Enchantment> enchantmentEntry = optional.get();
             if (enchantmentEntry.value().canEnchant(stack)) {
                 stack.enchant(enchantmentEntry, enchantmentEntry.value().getMaxLevel());
-
                 if (stackModifier != null) {
                     stackModifier.accept(stack);
                 }
-
                 entries.accept(stack);
             } else {
                 AtmosphericAPI.LOGGER.error("Avoided adding an ItemStack of {} because enchantment {} does not support that item", BuiltInRegistries.ITEM.getId(item), id);
             }
+
         } catch (Throwable throwable) {
             try {
                 AtmosphericAPI.LOGGER.error("Unable to add an ItemStack of {} because of an error when getting enchantment {}", BuiltInRegistries.ITEM.getId(item), enchantmentId, throwable);
