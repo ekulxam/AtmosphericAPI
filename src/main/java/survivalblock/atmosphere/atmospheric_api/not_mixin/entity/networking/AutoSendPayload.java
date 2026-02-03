b8cb0d8d679a959b91f07a6641bce7237dd18b3e
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2026-02-02 20:24:44 -0800

    Update some utilities
    Allow `AutoSendPayload#send` to accept a sync predicate
    Create `FunctionOperations` which is currently only used to chain `Predicate`s and related classes

--- src/main/java/survivalblock/atmosphere/atmospheric_api/not_mixin/entity/networking/AutoSendPayload.java
+++ src/main/java/survivalblock/atmosphere/atmospheric_api/not_mixin/entity/networking/AutoSendPayload.java
@@ -3,12 +3,23 @@ package survivalblock.atmosphere.atmospheric_api.not_mixin.entity.networking;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
+
+import java.util.function.Predicate;
 
 @SuppressWarnings("unused")
 public interface AutoSendPayload extends CustomPacketPayload {
 
     default void send(Entity entity) {
         PlayerLookup.tracking(entity).forEach(player -> ServerPlayNetworking.send(player, this));
+    }
+
+    default void send(Entity entity, Predicate<ServerPlayer> syncPredicate) {
+        PlayerLookup.tracking(entity).forEach(player -> {
+            if (syncPredicate.test(player)) {
+                ServerPlayNetworking.send(player, this);
+            }
+        });
     }
 }
---
+++ src/main/java/survivalblock/atmosphere/atmospheric_api/not_mixin/util/FunctionOperations.java
@@ -0,0 +1,40 @@
+package survivalblock.atmosphere.atmospheric_api.not_mixin.util;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.SpawnPlacements.SpawnPredicate;
+
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
+
+@SuppressWarnings("unused")
+public final class FunctionOperations {
+    private FunctionOperations() {
+    }
+
+    public static <T> Predicate<T> and(Predicate<? super T> one, Predicate<? super T> two) {
+        return (obj) -> one.test(obj) && two.test(obj);
+    }
+
+    public static <T> Predicate<T> or(Predicate<? super T> one, Predicate<? super T> two) {
+        return (obj) -> one.test(obj) || two.test(obj);
+    }
+
+    public static <T> Predicate<T> combine(Predicate<? super T> one, Predicate<? super T> two, BiFunction<Boolean, Boolean, Boolean> operator) {
+        return (obj) -> operator.apply(one.test(obj), two.test(obj));
+    }
+
+    public static <T extends Entity> SpawnPredicate<T> and(SpawnPredicate<T> one, SpawnPredicate<T> two) {
+        return (type, world, spawnReason, blockPos, random) ->
+                one.test(type, world, spawnReason, blockPos, random) && two.test(type, world, spawnReason, blockPos, random);
+    }
+
+    public static <T extends Entity> SpawnPredicate<T> or(SpawnPredicate<T> one, SpawnPredicate<T> two) {
+        return (type, world, spawnReason, blockPos, random) ->
+                one.test(type, world, spawnReason, blockPos, random) || two.test(type, world, spawnReason, blockPos, random);
+    }
+
+    public static <T extends Entity> SpawnPredicate<T> combine(SpawnPredicate<T> one, SpawnPredicate<T> two, BiFunction<Boolean, Boolean, Boolean> operator) {
+        return (type, world, spawnReason, blockPos, random) ->
+                operator.apply(one.test(type, world, spawnReason, blockPos, random), two.test(type, world, spawnReason, blockPos, random));
+    }
+}
