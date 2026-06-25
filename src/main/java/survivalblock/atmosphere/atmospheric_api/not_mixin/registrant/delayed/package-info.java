19381f8cdf6266a4c8bbc8dc1acb714dd13b2e0f
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2026-06-24 20:49:16 -0700

    Nuke registrant, use registrar (mod) now

--- src/main/java/survivalblock/atmosphere/atmospheric_api/not_mixin/registrant/delayed/package-info.java
+++
@@ -1,4 +0,0 @@
-@ApiStatus.Experimental
-package survivalblock.atmosphere.atmospheric_api.not_mixin.registrant.delayed;
-
-import org.jetbrains.annotations.ApiStatus;
--- src/main/java/survivalblock/atmosphere/atmospheric_api/not_mixin/registrant/package-info.java
+++
@@ -1,4 +0,0 @@
-@ApiStatus.Experimental
-package survivalblock.atmosphere.atmospheric_api.not_mixin.registrant;
-
-import org.jetbrains.annotations.ApiStatus;
